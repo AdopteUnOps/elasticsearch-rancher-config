0604699511a1218946e348b9923f89fd89e1c3cb
Jean-Louis Boudart <jeanlouis.boudart@gmail.com>
2017-09-18 14:44:04 +0200

    better handling of rack awareness

--- startup.sh
+++ startup.sh
@@ -4,36 +4,45 @@ set -e
 RANCHER_BASEURL="rancher-metadata.rancher.internal/latest"
 
 
-echo "installing custom elasticsearch config"
+echo "Installing custom elasticsearch config"
 mkdir -p /usr/share/elasticsearch/config
 # elasticsearch.yml
 curl -so /usr/share/elasticsearch/config/elasticsearch.yml ${RANCHER_BASEURL}/self/service/metadata/elasticsearch-config
 
-echo "adding rack awareness to elasticsearch config"
+echo "Adding rack awareness to elasticsearch config"
+
 # rack aware handling
-rack_aware_status_code=$(curl -so /dev/null -w  "%{http_code}" ${RANCHER_BASEURL}/hosts/0/labels/rack)
-
-if [ ${rack_aware_status_code} -eq 200 ]; then
-  rack_aware_uri="labels/rack"
-else
-  echo "no host labels 'rack' defined, will use hostname instead"
-  rack_aware_uri="hostname"
-fi
-
 nb_hosts=`curl --silent http://${RANCHER_BASEURL}/hosts | wc -l`
 echo "Parsing rack values from $nb_hosts hosts ..."
 rack_values=()
+hostname_values=()
 for (( i=0; i < ${nb_hosts}; i++ ))
 do
-   rack_values+=(`curl --silent "http://${RANCHER_BASEURL}/hosts/$i/${rack_aware_uri}"`)
+   rack_label=`curl --silent "http://${RANCHER_BASEURL}/hosts/$i/labels/rack"`
+   if [ "${rack_label}" != "Not found" ]; then
+      rack_values+=(rack_label)
+   fi
+   hostname_values+=(`curl --silent "http://${RANCHER_BASEURL}/hosts/$i/hostname"`)
 done
+
+#fallback to hostname if no rack labels
+use_hostname_as_rack_value=false
+if [ ${#rack_values[@]} -eq 0 ]; then
+  echo "no host labels 'rack' defined, will use hostname instead"
+  rack_values=$hostname_values
+  use_hostname_as_rack_value=true
+fi
 
 UNIQUE_RACK_VALUES=`printf "%s\n" "${rack_values[@]}" | sort -u | tr '\n' ',' | head -c-1`
 echo "Following rack values found on all hosts: $UNIQUE_RACK_VALUES"
 
-rack=`curl --silent http://${RANCHER_BASEURL}/self/host/${rack_aware_uri}`
+echo "Detecting current rack"
+if [ ${use_hostname_as_rack_value} ]; then
+  rack=`curl --silent http://${RANCHER_BASEURL}/self/host/hostname`
+else
+  rack=`curl --silent http://${RANCHER_BASEURL}/self/host/labels/rack`
+fi
 echo 'Current rack : ' $rack
-
 
 echo "
 cluster.routing.allocation.awareness.force.rack.values: \"${UNIQUE_RACK_VALUES}\"
